31e3b4db2a9e75e5b16a91ba19027d9231fb8bb8
Burlen Loring <bloring@lbl.gov>
2016-10-27 07:39:17 -0700

    update documentation, fix typo, fill in struct

--- data_access_layer.h
+++ data_access_layer.h
@@ -1,46 +1,68 @@
 #ifndef data_access_layer_h
 #define data_access_layer_h
 
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+
+
+// structure for shared data, a singleton instance, and
+// set/get api
 typedef struct
 {
+    uint32_t tick;
+    uint8_t alarm;
+    uint8_t units_toggle;
+    uint16_t depth;
+    uint16_t air_volume;
+    const char *brand_name;
+    uint8_t debug;
+}
+data_access_layer_t;
 
+data_access_layer *g_dal = NULL;
 
-
-} data_access_layer_t;
-
-
+// constructor
 data_access_layer _t *new_data_access_layer();
+
+// destructor
 void free_data_access_layer();
 
+// reset the data to initial values
 void dal_reset();
 
-
+// set/get the clock
 void dal_set_tick(uint32_t tick);
 uint32_t dal_get_tick(void);
 
+// set/get alarm state
 void dal_set_alarm(uint8_t val);
 uint8_t dal_get_alarm(void);
 
-void dal_set_uits_toggle(uint8_t val);
+// set/get the units
+#define UNITS_METRIC 0x01
+#define UNITS_US     0x02
+void dal_set_units_toggle(uint8_t val);
 uint8_t dal_get_units_toggle();
 
+// set/get the brand name strung
 void dal_set_brand_name(const char *vakl);
 const char *dal_get_brand_name();
 
+// set/get the depth rate
 void dal_set_depth_rate(int16_t val);
 int16_t dal_get_depth_rate();
 
+// set/get the depth rate
 void dal_set_depth(uint16_t val);
 uint16_t dal_get_depth();
 
-
+// set/get the current air volume
 void dal_set_air_volume(uint16_t val);
 uint16_t dal_get_air_volume();
 
+// set/get the debug flag state
 void dal_set_debug(uint8_t val);
 uint8_t dal_get_debug();
-
-void dal_set_();
-dal_get_();
 
 #endif
